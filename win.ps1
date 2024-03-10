edbe68f2b960b0aa71b349ab1c30aa4db8b0363c
lolali054 <62222309+lolali054@users.noreply.github.com>
2024-03-10 19:30:09 -0400

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/lolali054/Win/main/windowsactivation.cmd'
-$DownloadURL2 = 'https://raw.githubusercontent.com/lolali054/Win/main/windowsactivation.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/lolali054/Win/main/win.cmd'
+$DownloadURL2 = 'https://raw.githubusercontent.com/lolali054/Win/main/win.cmd'
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
